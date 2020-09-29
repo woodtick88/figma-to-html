e43abcf5b906d847eefabbf8324f5df5a4776a16
woodtick88 <woodtick88@gmail.com>
2020-09-29 13:16:21 +0300

    basket > selectric
    Корзина. Если товаров несколько, то "облагораживается" только первый селект с выбором цвета

--- app/js/basket/basket-main.js
+++ app/js/basket/basket-main.js
@@ -64,7 +64,7 @@ $(function() {
 
     // customize dropbox(select) in section .purchase
 
-	$('#parameter-colors-select').selectric({
+	$('.parameter-colors-select').selectric({
 		disableOnMobile: false,
 		nativeOnMobile: false
 	  });
@@ -74,12 +74,66 @@ $(function() {
 	// смена изображения при смене селекта
 
 
-	$("#parameter-colors-select").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".modelImg").hide(); // прячем все блоки с изображением 
+	$("#parameter-colors-select-1").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-1").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-1 .modelImg").hide(); // прячем все блоки с изображением 
 		$(img_id).show(); // показываем нужный блок с изображением
 	});
 
+
+	$("#parameter-colors-select-2").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-2").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-2 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
+
+	$("#parameter-colors-select-3").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-3").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-3 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
+
+	$("#parameter-colors-select-4").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-4").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-4 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
+
+	$("#parameter-colors-select-5").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-5").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-5 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
+
+	$("#parameter-colors-select-6").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-6").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-6 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
+
+	$("#parameter-colors-select-7").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-7").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-7 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
+
+	$("#parameter-colors-select-8").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-8").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-8 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
+
+	$("#parameter-colors-select-9").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-9").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-9 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
+
+	$("#parameter-colors-select-10").change(function(){ //При изменении модели
+		var img_id = $("#parameter-colors-select-10").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+		$(".colors-select-image-10 .modelImg").hide(); // прячем все блоки с изображением 
+		$(img_id).show(); // показываем нужный блок с изображением
+	});
 
 
 
