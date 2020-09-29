34b907d62070bab3873df04745cbce93dec10c29
woodtick88 <woodtick88@gmail.com>
2020-09-29 18:10:12 +0300

    правки
    передала селект на странице - basket.html
    
    Необходимо, чтобы в корзине
    - можно было удалить весь товар
    - если удалены все товары, то фома заказа должна убираться + вывести какую-нибудь надпись "Ваша корзина пуста. <a href="">В каталог</a>"
    - можно было удалить аксессуар
    - можно было изменить кол-во аксессуара. В 0 нельзя или, если убирается в 0, то строка аксессуара удалялась бы. Но лучше первое, чтобы избежать случайностей.
    - при этом всём пересчитывалась сумма всех товаров и аксессуаров.
    
    + валидация формы. Обязательные поля Имя + Телефон или Email

--- dist/js/basket/basket-main.js
+++ dist/js/basket/basket-main.js
@@ -74,66 +74,180 @@ $(function() {
 	// смена изображения при смене селекта
 
 
-	$("#parameter-colors-select-1").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-1").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-1 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
+	$(".parameter-colors-select").each(function() {
+
+
+		$(this).change(function(){ //При изменении модели
+			let selectData = $(this).children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
+			
+
+			$(this).parents('.parameter-colors').children('.select-image').children('.modelImg').each(function() {
+
+				if ( $(this).data('image') == selectData ) {
+					$(this).show()
+				} else {
+					$(this).hide();
+				}
+
+
+			})
+		});
+		
+
 	});
 
 
-	$("#parameter-colors-select-2").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-2").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-2 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
+	
+
+
+	// удалить единицу товара
+
+	$(".main-product-information .price .delete-item").click(function() {
+		
+		$(this).parents('.product-item-wrapper').remove();
+
+
+		// если нету выбраного товара - показ блока - Ваша корзина пуста
+
+		if ($(".product-item-wrapper").length){			
+		  
+		} else {
+			$('.purchase-wrapper.basket .purchase').hide();
+			$('.purchase-wrapper.basket .basket-empty').show();
+		}
+
+		basketTotalPrice();
+
+
 	});
 
-	$("#parameter-colors-select-3").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-3").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-3 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
+
+	// удалить параметр товара
+
+	$(".parameter .accessories-select li label .item-price .basket-trash-icon").click(function() {
+		
+		$(this).parents('.parameter .accessories-select li').remove();
+
+		basketTotalPrice();
+		
+
 	});
 
-	$("#parameter-colors-select-4").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-4").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-4 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
+
+	// подсчет итоговой сумы
+
+
+	let basketTotalPrice = function() {
+
+		let totalPrice = 0;
+
+		$('.product-item-wrapper input[type=checkbox]').each(function () {
+		
+			
+			let currenPrice = parseInt($(this).data('price')) * parseInt($(this).data('amount'));
+			totalPrice += parseInt($(this).data('price')) * parseInt($(this).data('amount'));
+
+			let firstPricePart = totalPrice.toString().slice(0, -3);
+			let secondPricePart = totalPrice.toString().slice(-3);
+
+		
+			$('.purchase-wrapper.basket .purchase .total-price .total').text(firstPricePart + ' ' + secondPricePart);
+
+
+		});
+
+
+	};
+
+	basketTotalPrice();
+
+
+
+	// счетчик аксесуаров
+
+
+	$('.parameter .accessories-select li label .price-select').each(function () {		
+		
+	
+		$(this).children('.minus').click(function() {
+
+			let currentNumber  =  parseInt( $(this).parents('.price-select').children('.curren-number').text() );
+			let currntDataAmount = $(this).parents(' li ').children('.accessories-checbox').data('amount');
+
+			if ( currentNumber <= 1 ) {
+				return
+			} else {
+				currentNumber = currentNumber - 1;				
+			}
+
+			$(this).parents('.price-select').children('.curren-number').text(currentNumber)
+			$(this).parents(' li ').children('.accessories-checbox').data('amount', currentNumber);
+
+			basketTotalPrice();
+			
+		});	
+		
+
+		$(this).children('.plus').click(function() {
+
+			let currentNumber  =  parseInt( $(this).parents('.price-select').children('.curren-number').text() );
+			
+			
+			currentNumber = currentNumber + 1;		
+
+			$(this).parents('.price-select').children('.curren-number').text(currentNumber)	
+			$(this).parents(' li ').children('.accessories-checbox').data('amount', currentNumber);
+
+			basketTotalPrice();
+
+		});		
+
 	});
 
-	$("#parameter-colors-select-5").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-5").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-5 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
-	});
 
-	$("#parameter-colors-select-6").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-6").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-6 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
-	});
 
-	$("#parameter-colors-select-7").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-7").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-7 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
-	});
+	// валидация полей формы
 
-	$("#parameter-colors-select-8").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-8").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-8 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
-	});
 
-	$("#parameter-colors-select-9").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-9").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-9 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
-	});
+	$('.purchase-wrapper.basket .purchase .basket-form').submit(function(e) {
+		e.preventDefault();
+		console.log(2)
 
-	$("#parameter-colors-select-10").change(function(){ //При изменении модели
-		var img_id = $("#parameter-colors-select-10").children("option:selected").attr("data"); // Получаем значение атрибута блока с изображением которое нужно показать на изображении
-		$(".colors-select-image-10 .modelImg").hide(); // прячем все блоки с изображением 
-		$(img_id).show(); // показываем нужный блок с изображением
-	});
+		let name = $('.basket-form-input.name').val();
+		let city = $('.basket-form-input.city').val();
+		let email = $('.basket-form-input.email').val();
+		let telefone = $('.basket-form-input.telefone').val();
+
+		$('.basket-form-input').removeClass('error');
+	 
+		if (name.length < 6) {
+			$('.basket-form-input.name').addClass('error');
+		}
+		if (city.length < 1) {
+			$('.basket-form-input.city').addClass('error');
+		}
+		if (email.length < 1) {
+		  $('.basket-form-input.email').addClass('error');
+		} else {
+		  let regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/;
+		  let validEmail = regEx.test(email);
+		  if (!validEmail) {
+			$('.basket-form-input.email').addClass('error');
+		  }
+		}
+		if (telefone.length < 1) {
+			$('.basket-form-input.telefone').addClass('error');
+		  } else {
+			let regEx = /^([+]?[0-9\s-\(\)]{3,25})*$/i;
+			let validTelefone = regEx.test(telefone);
+			if (!validTelefone) {
+			  $('.basket-form-input.telefone').addClass('error');
+			}
+		  }
+	  });
+
+
+		
 
 
 
